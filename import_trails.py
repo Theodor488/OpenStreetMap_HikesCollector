5420847c1bd2d76d93d91fd2a3b021738d7cfb19
Theodor Suciu <theodorsuciu@yahoo.com>
2025-02-12 06:01:32 -0800

    Parsing code and new export data

--- import_trails.py
+++ import_trails.py
@@ -3,6 +3,26 @@ import pyodbc
 from geopy.distance import geodesic
 
 
-with open("export.geojson", "r") as file:
+with open("export2.geojson", "r") as file:
     data = json.load(file)
-print(json.dumps(data["type"]))
+
+for i in range(len(data["features"])):
+    #name = json.dumps(data["features"][i]["properties"]["name"])
+    name = json.dumps(data["features"][i]["properties"].get("name", "Unknown"))
+    '''
+    if name == '"Unknown"':
+        name = json.dumps(data["features"][i]["properties"]["@relations"][0]["reltags"].get("name"))
+        alt_name = json.dumps(data["features"][i]["properties"]["@relations"][0]["reltags"].get("alt_name", "Unknown"))
+        if alt_name != '"Unknown"':
+            name = f"{name}: {alt_name}"
+    '''
+
+    print(f"{i}. {name}")
+
+# get all hike names
+#names = [data["name"] for data in data["features"]] 
+
+#print(names)
+
+
+# -121.49 / 47.37
